3fa70ec2fa5d84143cfbb62fbf5c09673ddf6f79
Grigorii Efimov <grinefimov@gmail.com>
2024-01-26 12:03:26 +0500

    Add SharedKernel solution

---
+++ src/SharedKernel/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedKernel;
+
+public abstract class EntityBase : HasDomainEventsBase
+{
+    public int Id { get; set; }
+}
+
+public abstract class EntityBase<TId> : HasDomainEventsBase
+    where TId : struct, IEquatable<TId>
+{
+    public TId Id { get; set; }
+}
---
+++ tests/SharedKernel.UnitTests/EntityBase_AddDomainEvent.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+
+namespace SharedKernel.UnitTests;
+
+public class EntityBase_AddDomainEvent
+{
+    private class TestDomainEvent : DomainEventBase { }
+
+    private class TestEntity : EntityBase
+    {
+        public void AddTestDomainEvent()
+        {
+            var domainEvent = new TestDomainEvent();
+            RegisterDomainEvent(domainEvent);
+        }
+    }
+
+    [Fact]
+    public void AddsDomainEventToEntity()
+    {
+        // Arrange
+        var entity = new TestEntity();
+
+        // Act
+        entity.AddTestDomainEvent();
+
+        // Assert
+        entity.DomainEvents.Should().HaveCount(1);
+        entity.DomainEvents.Should().AllBeOfType<TestDomainEvent>();
+    }
+}
