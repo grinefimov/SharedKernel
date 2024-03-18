3f4cb53f996491890a650ed2577326fd86778155
Grigorii Efimov <grinefimov@gmail.com>
2024-03-18 12:13:09 +0500

    Add OutboxMessage

---
+++ src/SharedKernel/Infrastructure/OutboxMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace SharedKernel.Infrastructure;
+
+public class OutboxMessage(int? id, DateTime occurredUtc, string type, string data, DateTime? processedUtc = null)
+{
+    public int? Id { get; init; } = id;
+    public DateTime OccurredUtc { get; init; } = occurredUtc;
+    public string Type { get; init; } = type;
+    public string Data { get; init; } = data;
+    public DateTime? ProcessedUtc { get; set; } = processedUtc;
+}
